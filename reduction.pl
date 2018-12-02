6b8d82eb63dfa5e84af9c8c9ee5ad4d9cade15f4
AlaaBenk94 <a.benkarrad@gmail.com>
2018-12-02 04:24:31 +0100

    corrections des tetes de clauses sur reduction.pl

--- reduction.pl
+++ reduction.pl
@@ -1,10 +1,10 @@
 %% le predicat de reduction qui transforme le systeme fourni en un nouveau systeme
 %% en appliquants une regle d'unification.
 
-reduit(R,rename,P,Q) :- .
-reduit(R,simplify,P,Q) :- .
-reduit(R,expand,P,Q) :- .
-reduit(R,check,P,Q) :- .
-reduit(R,decompose,P,Q) :- .
-reduit(R,orient,P,Q) :- .
-reduit(R,clash,P,Q) :- .
+reduit(rename,E,P,Q) :- .
+reduit(simplify,E,P,Q) :- .
+reduit(expand,E,P,Q) :- .
+reduit(check,E,P,Q) :- .
+reduit(decompose,E,P,Q) :- .
+reduit(orient,E,P,Q) :- .
+reduit(clash,E,P,Q) :- .
