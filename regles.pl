bee7a8b192e44937686d83edefa90eed940d7935
AlaaBenk94 <a.benkarrad@gmail.com>
2018-12-02 03:50:07 +0100

    defining occur_check + rules

--- regles.pl
+++ regles.pl
@@ -1,11 +1,12 @@
-%% le predicat qui verifie si une regle s'applique.
-regle(T1 ?= T2,rename) :- var(T1), var(T2).
-regle(T1 ?= T2,simplify) :- var(T1), nonvar(T2).
-%% regle(T1 ?= T2,expand) :- complex(T2), occur_check(T1,T2). % to review
-%% regle(T1 ?= T2,check) :- occur_check(T1,T2). % to review
-regle(T1 ?= T2,decompose) :- functor(T1,N,A), functor(T2,N,A), A\==0.
-regle(T1 ?= _,orient) :- nonvar(T1).
-regle(T1 ?= T2,clash) :- functor(T1,N1,A1), functor(T2,N2,A2), (N1\==N2;A1\==A2). 
+%% l'occur check
+occur_check(X,Y) :- occurrences_of_var(X,Y,Count), Count\==0.
 
-%% un terme complexe
-complex(T) :- functor(T,_,A), A\==0.
+%% le predicat qui verifie si une regle s'applique.
+regle(X ?= X, clean).
+regle(X ?= T,rename) :- var(X), var(T).
+regle(X ?= T,simplify) :- var(X), nonvar(T).
+regle(X ?= T,expand) :- compound(T), occur_check(X,T).
+regle(X ?= T,check) :- occur_check(X,T).
+regle(X ?= T,decompose) :- functor(X,N,A), functor(T,N,A), A\==0.
+regle(X ?= _,orient) :- nonvar(X).
+regle(X ?= T,clash) :- functor(X,Nx,Ax), functor(T,Nt,At), (Nx\==Nt;Ax\==At). 
